c12bf2fc6819ad60c0a364644fa70b5803357265
tinyverseapp <tinyverse.app@gmail.com>
2026-09-10 22:55:02 +0800

    feat(team): show realtime members of active team

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { AuthGate } from './components/AuthGate';
+import { TeamMembersPanel } from './components/TeamMembersPanel';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthGate>
       <App />
+      <TeamMembersPanel />
     </AuthGate>
   </StrictMode>,
 );
