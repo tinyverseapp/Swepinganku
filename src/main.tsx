307d77dc19414a2ccc0359353c9ab87dc4e9ab2d
tinyverseapp <tinyverse.app@gmail.com>
2026-09-10 23:05:47 +0800

    refactor: render team members inside topbar

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { AuthGate } from './components/AuthGate';
-import { TeamMembersPanel } from './components/TeamMembersPanel';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthGate>
       <App />
-      <TeamMembersPanel />
     </AuthGate>
   </StrictMode>,
 );
