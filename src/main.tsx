acb470c85c19b3da8c40563397cdc25996be4921
tinyverseapp <tinyverse.app@gmail.com>
2026-09-12 10:26:08 +0800

    Wire owner admin role into app

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { AuthGate } from './components/AuthGate';
+import { AdminOwnerShell } from './components/AdminOwnerShell';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthGate>
-      <App />
+      <AdminOwnerShell>
+        <App />
+      </AdminOwnerShell>
     </AuthGate>
   </StrictMode>,
 );
